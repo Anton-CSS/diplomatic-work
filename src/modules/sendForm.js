590983b7bd599e1cc68f958bf033600337ba9ccd
Anton Rokunets <9251580262@mail.ru>
2020-07-26 22:10:13 +0300

    footerForm

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -14,8 +14,8 @@ const sendForm = () => {
   bannerForm = document.getElementById('banner-form'),
   banner = document.getElementById('banner'),
   cardCheck = document.getElementById('card_check'),
-  bannerBtn = document.getElementById('banner-btn'),
   check = document.getElementById('check'),
+  check1 = document.getElementById('check1'),
   check2 = document.getElementById('check2'),
   input1 = cardOrder.querySelector('input[name="name"]'),
   input2 = cardOrder.querySelector('input[name="phone"]'),
@@ -54,8 +54,6 @@ const sendForm = () => {
   };
 
 
-  
-
   form.forEach((item) =>{
     item.addEventListener('submit', (event) => {
       
@@ -63,46 +61,7 @@ const sendForm = () => {
         document.querySelector('.required').remove();
       }
 
-      if(event.target === footerForm ){
-        if (footerLetoMozaika.checked || footerLetoSchelkovo.checked) {
-          
-          event.preventDefault();
-          const formData = new FormData(item);
-          let body = {};
-          formData.forEach((val, key) => {
-            body[key] = val;
-          });
-
-          if (footerLetoMozaika.checked) {
-            body['club-name'] = 'mozaika';
-          } else {
-            body['club-name'] = 'schelkovo';
-          }
-
-          const input = footerForm.querySelectorAll('input');
-          input.forEach(elem => elem.value = '');
-
-
-          postData(body)
-            .then((response) => {
-              if (response.status !== 200) {
-                throw new Error('status network not 200');
-              }
-              thanks.classList.toggle('active');
-            })
-            .catch((error) => {
-              thanks.classList.toggle('active');
-              formContent.innerHTML = `<h4>Ошибка</h4>
-          <p style="margin: auto">Попробуйте отправить сообщение позже</p>
-          `
-             console.log(error);
-            });
-        } else{
-          event.preventDefault();
-        }
-        
-        
-      } else if(event.target === cardOrder){
+      if(event.target === cardOrder){
         if (cardCheck.checked === true) {
           if(input1.value === '' || input2.value === ''){
             event.preventDefault();
@@ -158,52 +117,80 @@ const sendForm = () => {
              event.preventDefault();
         }
 
-      } else if(event.target === form2 || event.target === form1|| event.target === bannerForm){
+      } else if(event.target === form2 || event.target === form1|| event.target === bannerForm || event.target === footerForm ){
         
         let target = event.target;
         
-          if(check2.checked || check.checked || check1.checked){
+          if(check2.checked || check.checked || check1.checked || footerLetoMozaika.checked || footerLetoSchelkovo.checked){
             
-            const callbackFormPhone = target.querySelector('input[name="phone"]'),
-                  callbackFormName = target.querySelector('input[name="name"]');
-            if(callbackFormPhone.value === '' || callbackFormName.value === ''){
+            const formPhone = target.querySelector('input[name="phone"]'),
+                  formName = target.querySelector('input[name="name"]');
+              
+            if(formPhone.value === '' ){
               event.preventDefault();
               
             } else{
-              event.preventDefault();
-              check2.checked = false;
-              check.checked = false;
-              target.appendChild(statusMessage);
-              
-              statusMessage.style.display = 'block';
-      
-              const formData = new FormData(target);
-              let body = {};
-              formData.forEach((val, key) =>{
-              body[key] = val;
-              });
-      
-              const input = target.querySelectorAll('input');
-              input.forEach(elem => elem.value = '');
-      
-              postData(body) 
-                .then((response) => {
-                  if(response.status !== 200){
-                    throw new Error('status network not 200');
+              if(formName && formName.value === ''){
+                event.preventDefault();
+              } else{
+                event.preventDefault();
+                footerLetoSchelkovo.checked = false;
+                check1.checked = false;
+                check2.checked = false;
+                check.checked = false;
+                target.appendChild(statusMessage);
+                statusMessage.style.display = 'block';
+
+                const formData = new FormData(target);
+                let body = {};
+                formData.forEach((val, key) => {
+                  body[key] = val;
+                });
+                console.log(footerLetoMozaika.checked);
+                if(target === footerForm && footerLetoMozaika.checked) {
+                    body['club-name'] = 'mozaika';
+                  } else {
+                    body['club-name'] = 'schelkovo';
                   }
-                statusMessage.style.display = 'none';
-                target.appendChild(message);
-                setTimeout(noMessage, 3000);
-              }) 
-              .catch ((error) => {
-                statusMessage.style.display = 'none';
-                message.textContent = 'ОШИБКА';
-                target.appendChild(message);
-                setTimeout(noMessage, 3000);
-                console.log(error);
-              });
+                footerLetoMozaika.checked = false;
+
+                const input = target.querySelectorAll('input');
+                input.forEach(elem => elem.value = '');
+                
+
+                postData(body)
+                  .then((response) => {
+                    if (response.status !== 200) {
+                      throw new Error('status network not 200');
+                    }
+                    if (target === footerForm) {
+                      thanks.classList.toggle('active');
+                      statusMessage.style.display = 'none';
+                    } else {
+                      statusMessage.style.display = 'none';
+                      target.appendChild(message);
+                      setTimeout(noMessage, 3000);
+                    }
+
+                  })
+                  .catch((error) => {
+                    if (target === footerForm) {
+                      thanks.classList.toggle('active');
+                      formContent.innerHTML = `<h4>Ошибка</h4>
+                     <p style="margin: auto">Попробуйте отправить сообщение позже</p>
+                     `
+                      console.log(error);
+                    } else {
+                      statusMessage.style.display = 'none';
+                      message.textContent = 'ОШИБКА';
+                      target.appendChild(message);
+                      setTimeout(noMessage, 3000);
+                      console.log(error);
+                    }
+                  });
+              }
             }
-          } else if(check1.checked === false){
+          } else if(check1.checked === false ){
               let bannerMessage = document.createElement('div');
               bannerMessage.textContent = 'Согласие на обработку данных обязательно'.toLocaleUpperCase();
               bannerMessage.setAttribute('class', 'required');
@@ -214,41 +201,9 @@ const sendForm = () => {
           }else{
             event.preventDefault();
           }
-
-      }else{
-
-        event.preventDefault();
-        item.appendChild(statusMessage);
-        statusMessage.style.display = 'block';
-
-        const formData = new FormData(item);
-        let body = {};
-        formData.forEach((val, key) =>{
-        body[key] = val;
-        });
-
-        const input = item.querySelectorAll('input');
-        input.forEach(elem => elem.value = '');
-
-        postData(body) 
-          .then((response) => {
-            if(response.status !== 200){
-              throw new Error('status network not 200');
-            }
-          statusMessage.style.display = 'none';
-          item.appendChild(message);
-          setTimeout(noMessage, 3000);
-        }) 
-        .catch ((error) => {
-          statusMessage.style.display = 'none';
-          message.textContent = 'ОШИБКА';
-          item.appendChild(message);
-          setTimeout(noMessage, 3000);
-          console.log(error);
-        });
       }
     });
   });
-}
+};
 
 sendForm();
